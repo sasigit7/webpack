18710e1349e644ad3e9246103eb70071c526db33
Shashi <sasiwebdev7@gmail.com>
2021-12-19 13:53:11 +0000

    Using Webpack to bundle our JS files

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class MyComponent extends React.Component {
   render() {
-   // return <h1>{this.props.title}}</h1>;
+  // return <h1>{this.props.title}}</h1>;
    return React.createElement('h1', null, `${this.props.title}`);
   }
 }
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+}
+
